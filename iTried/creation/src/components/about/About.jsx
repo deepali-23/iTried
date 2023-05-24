73ef2d1e35066a5ffeedabefb12ea116fd4af6f4
deepali-23 <sharma.deepali1999@gmail.com>
2023-05-24 15:57:25 +0530

    change

--- iTried/creation/src/components/about/About.jsx
+++ iTried/creation/src/components/about/About.jsx
@@ -25,7 +25,7 @@ const About = () => {
             <article className="about__card">
               <FiUsers className="about__icons" />
               <h5>Clients</h5>
-              <small>200+ Worldwides </small>
+              <small>200+ Worldwide </small>
             </article>
             <article className="about__card">
               <VscFolderLibrary className="about__icons" />
@@ -33,16 +33,12 @@ const About = () => {
               <small>4+ completed Projects</small>
             </article>
             <p>
-              Lorem Ipsum is simply dummy text of the printing and typesetting
-              industry. Lorem Ipsum has been the industry's standard dummy text
-              ever since the 1500s, when an unknown printer took a galley of
-              type and scrambled it to make a type specimen book. It has
-              survived not only five centuries, but also the leap into
-              electronic typesetting, remaining essentially unchanged. It was
-              popularised in the 1960s with the release of Letraset sheets
-              
+              Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy
+              text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has
             </p>
-            <a href="#contact" className='btn btn-primary'>Let's Talk</a>
+            <a href="#contact" className="btn btn-primary">
+              Let's Talk
+            </a>
           </div>
         </div>
       </div>
